7689b198e8f369eb304ac0845168b97e3b35c8c0
cleytonfs777 <cleyton.fs777@gmail.com>
2023-05-10 23:16:31 -0300

    correcao de static

---
+++ templates/static/disparo/js/envio.js
@@ -0,0 +1,12 @@
+function confirmar() {
+    showLoadingIndicator()
+    document.getElementById('form_confirmar').submit();
+}
+
+function cancelar() {
+    window.location.href = enviar_disparo_url;
+}
+
+
+     
+
